d0d7d077b162e12e74ca5fad948332cf984f0abf
robkau <robkau@users.noreply.github.com>
2022-04-02 13:05:39 -0400

    bump down spawn frequency to fit in usize in WASM compilation.

--- src/spawners.rs
+++ src/spawners.rs
@@ -73,7 +73,7 @@ pub(super) fn create_initial_spawners(mut commands: Commands, grid: Res<Grid>) {
         ParticleSpawnerInfo {
             created_at: 0,
             pattern: SpawnerPattern::Tower,
-            spawn_frequency: 999999999999,
+            spawn_frequency: 99999999,
             max_particles: 50000,
             particle_duration: 500000,
             particle_origin: Vec2::new(3. * grid.width as f32 / 4., 1.),
