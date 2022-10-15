aa06b530cac039eb8403afaa7f424a1cc5e97946
robkau <robkau@users.noreply.github.com>
2022-10-15 12:21:22 -0400

    fix predictive boundary conditions. lower default dt for stability.

--- src/defaults.rs
+++ src/defaults.rs
@@ -8,7 +8,7 @@ pub(super) const DEFAULT_GRID_WIDTH: usize = usize::pow(2, 8);
 // pub(super) const PARTICLES_ACROSS_GRID: usize = 1024;
 // pub(super) const PARTICLES_ACROSS_CELL: usize = PARTICLES_ACROSS_GRID / DEFAULT_GRID_WIDTH;
 
-pub(super) const DEFAULT_DT: f32 = 0.0025;
+pub(super) const DEFAULT_DT: f32 = 0.002;
 pub(super) const DEFAULT_GRAVITY: f32 = -9.8;
 
 pub(super) const PAR_BATCH_SIZE: usize = usize::pow(2, 12);
